b2540d05e7bc3098cd6f7fc4460c408b20616cd9
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2023-06-13 18:56:02 -0500

    fixed import

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"go_blockchain/blockchain"
 )
 
 
+
 func main () {
-	chain := InitBlockchain()
+	chain := blockchain.InitBlockchain()
 
 	chain.AddBlock("Second block")
 	chain.AddBlock("Third block")
 	chain.AddBlock("Fourth block")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("PreviousHash: %x\n", block.Prevhash )
 		fmt.Printf("Block data: %s\n", block.Data )
 		fmt.Printf("Hash: %x\n", block.Prevhash )
